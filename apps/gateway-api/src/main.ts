d5770deaaef41f2edec7e281c20e70826e4b9738
mtt-wlsn <74565505+mtt-wlsn@users.noreply.github.com>
2023-02-22 13:10:20 -0600

    Removed gateway.

--- apps/gateway-api/src/main.ts
+++
@@ -1,9 +0,0 @@
-import '@trace/tracing';
-import { NestFactory } from '@nestjs/core';
-import { GatewayApiModule } from './gateway-api.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(GatewayApiModule);
-  await app.listen(process.env.GATEWAY_API_PORT);
-}
-bootstrap();
